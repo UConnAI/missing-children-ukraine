d7261b1b73a6ae109993ef256e57b575bcb56713
m4nik1 <maniksoomro@hotmail.com>
2022-04-26 22:43:17 -0400

    Fixing stuff

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../CSS/App.css'
+
+function Home() {
+  return (
+    <div className="App">
+      <div className='Text-Container'>
+        <p>Report missing children through Sylnishi to alert Europe's Missing Children experts who will use their contacts with the local authorities and the Interpol to help you find your child. </p>
+      </div>
+      <button className='Startbtn'>Let's Start</button>
+    </div>
+  );
+}
+
+export default Home;
