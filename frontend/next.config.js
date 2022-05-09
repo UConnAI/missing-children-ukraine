128b8b4d9c24e6a33b49affffa203964856aaeb7
m4nik1 <maniksoomro@hotmail.com>
2022-05-09 11:29:57 -0400

    Migrated to nextjs pwa

---
+++ frontend/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register:true,
+    skipWaiting:true,
+    // disable: process.env.NODE_ENV === "development"
+  }
+})
